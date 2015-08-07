963c02465bbaa50cfd0b69d5bc326acd6aa785fc
Henri Bergius <henri.bergius@iki.fi>
2015-08-07 06:53:47 -0400

    Start servers for the browser tests

--- spec/utils/echoserver.js
+++ spec/utils/echoserver.js
@@ -2,5 +2,5 @@ require('coffee-script/register');
 var utils = require('./utils');
 var port = parseInt(process.argv[2]);
 utils.createServer(port, function(err, server) {
-	console.log("Echo server running on " + port);
+  console.log("Echo server running on " + port);
 });
--- spec/utils/nofloserver.js
+++ spec/utils/nofloserver.js
@@ -2,5 +2,5 @@ require('coffee-script/register');
 var utils = require('./utils');
 var port = parseInt(process.argv[2]);
 utils.createNoFloServer(port, function(err, server) {
-	console.log("Echo server running on " + port);
+  console.log("NoFlo server running on " + port);
 });
